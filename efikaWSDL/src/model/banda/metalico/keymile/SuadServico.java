ac96ef48b59f18a1a1b236e4c039a791e6d89d5e
henmerlin <henmerlin@gmail.com>
2016-03-10 20:02:27 -0300

    Agi edition

--- efikaWSDL/src/model/banda/metalico/keymile/SuadServico.java
+++ efikaWSDL/src/model/banda/metalico/keymile/SuadServico.java
@@ -10,7 +10,13 @@ public class SuadServico {
 	public SuadServico() {
 		
 	}
-	
+	/*
+	 * Retorna:
+	 * 	velocidade do canal, em kbps;
+	 * 	sinal ruído da porta, em dB;
+	 * 	atenuação da porta, em dB; 
+	 * 
+	 */
 	public String consultarParametros(GetInfoOut cadastro) {
 		
 		InfoTBS tbs = cadastro.getInfoTBS();
@@ -19,9 +25,47 @@ public class SuadServico {
 		
 		telnet.setAuth(DslamFactory.keymileCredencial());
 		telnet.setIp(tbs.getIpDslam());
-				
+		
+		String comando1 = "get /unit-" + tbs.getSlot() + "/port-" + tbs.getPortNumber() + "/chan-1/status/status";
+		String comando2 = "get /unit-" + tbs.getSlot() + "/port-" + tbs.getPortNumber() + "/status/snrmargin";
+		String comando3 = "get /unit-" + tbs.getSlot() + "/port-" + tbs.getPortNumber() + "/status/attenuation";
+		
 		
 		return null;
 	}
-
+	/*
+	 * Retorna o estado administrativo da porta 	
+	 */
+	public String adminStatus(GetInfoOut cadastro){
+		
+		InfoTBS tbs = cadastro.getInfoTBS();
+		
+		String comando =  "get /unit-" + tbs.getSlot() + "/port-" + tbs.getPortNumber() + "/main/administrativestatus";
+		
+		return null;
+	}
+	/*
+	 * Retorna o estado operacional da porta 	
+	 */
+	public String operStatus(GetInfoOut cadastro){
+		
+		InfoTBS tbs = cadastro.getInfoTBS();
+		
+		String comando =  "get /unit-" + tbs.getSlot() + "/port-" + tbs.getPortNumber() + "/main/operationalstatus";
+		
+		return null;
+	}
+	/*
+	 * Retorna o estado operacional da porta 	
+	 */
+	public String chanSrvcs(GetInfoOut cadastro){
+		
+		InfoTBS tbs = cadastro.getInfoTBS();
+		
+		String comando =  "ls /unit-" + tbs.getSlot() + "/port-" + tbs.getPortNumber() + "/chan-1/";
+		
+		return null;
+	}
+	
+	
 }
