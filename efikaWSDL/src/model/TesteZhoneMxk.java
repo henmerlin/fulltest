fb60109e86a0f71804fa7308b02f3f5662c8dce4
G0034481 <G0034481@CTARO8-5001.gvt.net.br>
2016-04-07 12:52:19 -0300

    Commit MXK Finale

---
+++ efikaWSDL/src/model/TesteZhoneMxk.java
@@ -0,0 +1,42 @@
+package model;
+
+import entidades.banda.parametros.TabelaParametrosMetalico;
+import model.banda.metalico.zhone.MxkServico;
+
+
+public class TesteZhoneMxk {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		try {
+			
+			MxkServico mxk = new MxkServico();			
+			TabelaParametrosMetalico tabela = mxk.consultarTabelaParametros();					
+
+		/*	System.out.println(" ");
+			System.out.println("SUAD");
+			System.out.println(" ");
+			
+			System.out.println("Adm Status -> " + tabela.getPortaAdmStatus());
+			System.out.println("Sinc Status -> " + tabela.getSincronismoStatus());
+			System.out.println("Modulação -> " + tabela.getModulacao());
+			System.out.println("Perfil -> " + tabela.getProfile());
+			System.out.println("Vel. Download -> " + tabela.getDownload());
+			System.out.println("Vel. Upload -> " + tabela.getUpload());
+			System.out.println("SnrDown  -> " + tabela.getSnrDown());
+			System.out.println("SnrUp -> " + tabela.getSnrUp());
+			System.out.println("AtnDown  -> " + tabela.getAtnDown());
+			System.out.println("AtnUp -> " + tabela.getAtnUp());*/
+			
+						
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		
+		
+	}
+}
+
--- efikaWSDL/src/model/banda/metalico/zhone/MxkServico.java
+++ efikaWSDL/src/model/banda/metalico/zhone/MxkServico.java
@@ -13,7 +13,7 @@ import util.TelnetUtil;
 public class MxkServico extends ZhoneServico{
 
 	public MxkServico() {
-		
+
 	}
 	/*
 	 * Retorna:
@@ -25,60 +25,89 @@ public class MxkServico extends ZhoneServico{
 	 * 		pacotes enviados
 	 * 		crc (up ou down ou a media dos dois, desconhecido)
 	 */
-	
+
 	public TabelaParametrosMetalico consultarTabelaParametros() throws Exception{
 
 		InfoTBS tbs = new InfoTBS();
 
-		tbs.setIpDslam("10.171.33.102");
-		tbs.setSlot(new BigInteger("16"));
-		tbs.setPortNumber(new BigInteger("22"));
-		tbs.setPortAddrSeq(new BigInteger("1430"));
-		
+		tbs.setIpDslam("10.131.113.31");
+		tbs.setSlot(new BigInteger("8"));
+		tbs.setPortNumber(new BigInteger("10"));
+		tbs.setPortAddrSeq(new BigInteger("1226"));
+
 		this.getTelnet().setIp(tbs.getIpDslam());
-		
+
 		this.getTelnet().getComandos().add(new ComandoTelnet(this.cmdPortStatus(tbs)));
 		this.getTelnet().getComandos().add(new ComandoTelnet("A"));
 		
+		this.getTelnet().getComandos().add(new ComandoTelnet(this.cmdModulacao(tbs)));
+		
+		this.getTelnet().getComandos().add(new ComandoTelnet(this.profileDown(tbs)));
+
 		this.getTelnet().setMode(ExecutionType.ZHONE_MXK);
-		
+
 		ArrayList<String> retorno = (ArrayList<String>) this.getTelnet().run();
-		
+
 		TabelaParametrosMetalico tabela = new TabelaParametrosMetalico();
 
+		// Status Port
+		tabela.setPortaAdmStatus(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "AdminStatus", 1))));
+		tabela.setSincronismoStatus(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "LineStatus", 1))));
+
+		// Velocidade Sincronizada
+		tabela.setDownload(new Double(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "DslDownLineRate", 1)))) /1000);
+		tabela.setUpload(new Double(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "DslUpLineRate", 1)))) /1000);
+
+		// SNR
+		tabela.setSnrDown(new Double(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "curSnrMargin", 2)))) /10);
+		tabela.setSnrUp(new Double(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "DslLineSnrMgn", 1)))) /10);
+		
+		
+		// ATN
+		tabela.setAtnDown(new Double(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "currAtn", 2)))) /10);
+		tabela.setAtnUp(new Double(TelnetUtil.tratamentoStringZhone(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "DslLineAtn", 1)))) /10);
+		
+		// Modulação
+		tabela.setModulacao(TelnetUtil.tratamentoStringZhoneDif(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "transmit-mode", 1))));
+		
+		//Profile da porta Down/Up
+		Double profileDown = new Double(TelnetUtil.tratamentoStringZhoneDif(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "interleaveMaxTxRate", 1))));
+		Double profileUp = new Double(TelnetUtil.tratamentoStringZhoneDif(retorno.get(TelnetUtil.posicaoArrayDeSubString(retorno, "fastMaxTxRate", 1))));
+				
+		tabela.setProfile(profileDown + " - " + profileUp);
+		
+		System.out.println(tabela.getProfile());		
+		
 		TelnetUtil.debugger(retorno);
 
 		return tabela;
 	}
-	
+
 	public String cmdPortStatus(InfoTBS tbs){
-		
+
 		return "dslstat 1/" + tbs.getSlot() + "/" + tbs.getPortNumber() + "/0/vdsl -v";
 	}
 	/*
-	 * Retorna o profile de down da porta
+	 * Retorna o profile de down/up da porta
 	 */
-	public String profileDown(GetInfoOut cadastro){
-		
-		InfoTBS tbs = cadastro.getInfoTBS();
-		
-		String comando = "get vdsl-co-config 1/" + tbs.getSlot() + "/" +tbs.getPortNumber();
-		
-		return null;
+	public String profileDown(InfoTBS tbs){
+
+		return "get vdsl-co-config 1/" + tbs.getSlot() + "/" + tbs.getPortNumber() + "/0/vdsl";
 	}
 	/*
 	 * Retorna o profile de up da porta
 	 */
 	public String cmdProfileUp(InfoTBS tbs){
-				
+
 		return "get vdsl-cpe-config 1/" + tbs.getSlot() + "/" +tbs.getPortNumber();
 	}
 	/*
 	 * Retorna a modulação da porta
 	 */
-	public String cmdModulacao(InfoTBS tbs){
-					
-		return "get vdsl-config 1/" + tbs.getSlot() + "/" +tbs.getPortNumber();
+	public String cmdModulacao(InfoTBS tbs){	
+		
+		return "get vdsl-config  1/" + tbs.getSlot() + "/" + tbs.getPortNumber() + "/0/vdsl";
+		
 	}
 
 }
