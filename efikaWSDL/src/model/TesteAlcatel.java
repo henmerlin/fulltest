69738af3f6472f932d46f3f2417ae6755f7c27f0
G0034481 <G0034481@CTARO8-5001.gvt.net.br>
2016-04-08 15:09:01 -0300

    commit

---
+++ efikaWSDL/src/model/TesteAlcatel.java
@@ -0,0 +1,37 @@
+package model;
+
+import entidades.banda.parametros.TabelaParametrosGpon;
+import model.banda.gpon.alcatel.AlcatelServico;
+
+
+public class TesteAlcatel {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		try {
+			
+			AlcatelServico alcatel = new AlcatelServico();			
+			TabelaParametrosGpon tabela = alcatel.consultarTabelaParametros();		
+
+			/*System.out.println("Adm Status -> " + tabela.getPortaAdmStatus());
+			System.out.println("Sinc Status -> " + tabela.getSincronismoStatus());
+			System.out.println("Modulação -> " + tabela.getModulacao());
+			System.out.println("Perfil -> " + tabela.getProfile());
+			System.out.println("Vel. Download -> " + tabela.getDownload());
+			System.out.println("Vel. Upload -> " + tabela.getUpload());
+			System.out.println("SnrDown  -> " + tabela.getSnrDown());
+			System.out.println("SnrUp -> " + tabela.getSnrUp());
+			System.out.println("AtnDown  -> " + tabela.getAtnDown());
+			System.out.println("AtnUp -> " + tabela.getAtnUp());*/		
+					
+
+		} catch (Exception e) {
+			//System.out.println(e.getMessage());
+		}
+
+		
+		
+	}
+}
+
