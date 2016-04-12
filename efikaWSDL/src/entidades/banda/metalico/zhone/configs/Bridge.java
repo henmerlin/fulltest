48d1d33d2e25f207c5612fa731ac473cae6e0e66
G0034481 <G0034481@CTARO8-5001.gvt.net.br>
2016-04-12 11:37:24 -0300

    commit

---
+++ efikaWSDL/src/entidades/banda/metalico/zhone/configs/Bridge.java
@@ -0,0 +1,59 @@
+package entidades.banda.metalico.zhone.configs;
+
+public class Bridge {
+	
+	private String slot;
+	
+	private String port;
+	
+	private String endSeqPort;
+	
+	private String vc;
+	
+	private String rin;
+	
+	public Bridge() {
+		
+	}
+
+	public String getSlot() {
+		return slot;
+	}
+
+	public void setSlot(String slot) {
+		this.slot = slot;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	public String getEndSeqPort() {
+		return endSeqPort;
+	}
+
+	public void setEndSeqPort(String endSeqPort) {
+		this.endSeqPort = endSeqPort;
+	}
+
+	public String getVc() {
+		return vc;
+	}
+
+	public void setVc(String vc) {
+		this.vc = vc;
+	}
+
+	public String getRin() {
+		return rin;
+	}
+
+	public void setRin(String rin) {
+		this.rin = rin;
+	}
+
+}
