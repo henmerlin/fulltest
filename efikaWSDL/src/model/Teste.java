efe492eeae6cfc9ca95f29a6b94d653c5e08e72a
henmerlin <henmerlin@gmail.com>
2016-04-12 16:44:41 -0300

    add socket

--- efikaWSDL/src/model/Teste.java
+++ efikaWSDL/src/model/Teste.java
@@ -1,5 +1,7 @@
 package model;
 
+import entidades.banda.parametros.TabelaHistorico;
+import entidades.banda.parametros.TabelaParametrosMetalico;
 import entidades.cadastro.Cadastro;
 import entidades.cadastro.CadastroMetalico;
 import model.banda.metalico.keymile.SuadServico;
@@ -10,10 +12,24 @@ public class Teste {
 		
 		
 		SuadServico suad = new SuadServico();	
-			
-		Cadastro cadastro = new CadastroMetalico();
 		
-		suad.consultarBridges(cadastro);
+		try {
+			
+			Cadastro cadastro = new CadastroMetalico();
+			
+			TabelaParametrosMetalico oi = suad.consultarTabelaParametros(cadastro);
+			
+			TabelaHistorico tabela = suad.consultarTabelaHistorico();
+			
+			System.out.println(tabela.getDias().get(0).getPcktsDown());
+			System.out.println(oi.getPortaAdmStatus());
+			
+			suad.disconnect();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+			
 
 
 	}
