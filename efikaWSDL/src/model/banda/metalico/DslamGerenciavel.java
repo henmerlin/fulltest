6beef51d10e19029868f645558170f824419f196
henmerlin <henmerlin@gmail.com>
2016-04-12 15:24:54 -0300

    Alteração extends  DslamGerênciavel

--- efikaWSDL/src/model/banda/metalico/DslamGerenciavel.java
+++ efikaWSDL/src/model/banda/metalico/DslamGerenciavel.java
@@ -1,5 +1,7 @@
 package model.banda.metalico;
 
+import bean.ossturbonet.oss.gvt.com.GetInfoOut;
+import bean.ossturbonet.oss.gvt.com.InfoTBS;
 import model.telnet.SocketClass;
 import model.telnet.Telnet;
 
@@ -8,7 +10,12 @@ public abstract class DslamGerenciavel {
 	private Telnet telnet;
 	
 	private SocketClass socket;
-		
+	
+	private GetInfoOut getInfo;
+	
+	@SuppressWarnings("unused")
+	private InfoTBS tbs;
+
 	
 	public DslamGerenciavel() {
 		this.telnet = new Telnet();
@@ -30,4 +37,18 @@ public abstract class DslamGerenciavel {
 	public void setSocket(SocketClass socket) {
 		this.socket = socket;
 	}
+	
+	public InfoTBS getTbs() {
+		return this.getInfo.getInfoTBS();
+	}
+
+	public GetInfoOut getGetInfo() {
+		return getInfo;
+	}
+
+	public void setGetInfo(GetInfoOut getInfo) {
+		this.getInfo = getInfo;
+	}
+	
+	
 }
--- efikaWSDL/src/model/banda/metalico/DslamMetalico.java
+++ efikaWSDL/src/model/banda/metalico/DslamMetalico.java
@@ -1,20 +1,6 @@
 package model.banda.metalico;
 
-import bean.ossturbonet.oss.gvt.com.GetInfoOut;
 
 public abstract class DslamMetalico extends DslamGerenciavel{
 
-	private GetInfoOut tbs;
-	
-	public DslamMetalico() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public GetInfoOut getTbs() {
-		return tbs;
-	}
-
-	public void setTbs(GetInfoOut tbs) {
-		this.tbs = tbs;
-	}
 }
