7be4755dfb1818c05757eeacb4cd8b3903077cc4
G0034481 <G0034481@CTARO8-5001.gvt.net.br>
2016-05-12 12:10:19 -0300

    Commit finale Upload

--- efikaWSDL/src/entidades/massivo/Status.java
+++ efikaWSDL/src/entidades/massivo/Status.java
@@ -61,4 +61,34 @@ public class Status {
 	public void setLotes(List<Lote> lotes) {
 		this.lotes = lotes;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Status other = (Status) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Status [id=" + id + "]";
+	}	
 }
